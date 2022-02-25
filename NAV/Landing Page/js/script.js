c1e4a9e1fc3d0c36851c27de5168542873b7d506
rohitharya <rohitharya19@gmail.com>
2022-02-25 20:48:24 +0530

    first comit

---
+++ NAV/Landing Page/js/script.js
@@ -0,0 +1,40 @@
+
+
+let alta = document.getElementById('alta');
+let background = document.getElementById('background');
+let title = document.getElementById('title');
+
+
+
+function rosegoldColor(){
+	alta.src = 'image/pic_001.png';
+	background.style.backgroundColor = '#3E85B9';
+	title.style.color = '#3E85B9';
+}
+
+
+function coralColor(){
+	alta.src = 'image/pic_007.png';
+	background.style.backgroundColor = '#E86A54';
+	title.style.color = '#E86A54';
+}
+
+
+function fuschiaColor(){
+	alta.src = 'image/pic_008.png';
+	background.style.backgroundColor = '#5D6F63';
+	title.style.color = '#5D6F63';
+}
+
+
+function blackColor(){
+	alta.src = 'image/pic_005.png';
+	background.style.backgroundColor = '#C6AD61';
+	title.style.color = '#C6AD61';
+}
+
+
+
+
+
+
